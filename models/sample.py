4b4e4a6a3256abec34034dbdcb4659afc681718b
Louis Bradshaw <Louisb19@live.co.uk>
2023-03-09 17:49:34 +0000

    added random sampling
    Former-commit-id: 8b34e5257a4c933f098f3ca427825dc6acacadeb [formerly 14c8c738119f894469b63e8da6c22a23af4905b9]
    Former-commit-id: cfb397cd2acad64d605f035ae3cd807496124552

--- models/sample.py
+++ models/sample.py
@@ -2,6 +2,7 @@ import os
 import math
 import json
 import torch
+import random
 from midiutil import MIDIFile
 from .model import ChoraleBertModel
 from .dataset import ChoraleDataset
@@ -20,7 +21,7 @@ class Sampler():
         self.save_path = save_path
         
         model.eval()
-        self._gen_fugues('./data/processed/fugue16sep64len_prompts.json', 12)
+        self._gen_fugues_autoreg('./data/processed/fugue16sep64len_prompts.json', 6)
         #self._test_set(50)
         
     def _test_set(self, num_samples: int):
@@ -53,7 +54,124 @@ class Sampler():
 
             print(f'Completed {i}/{num_samples}')
 
-    def _gen_fugues(self, load_path: str, num_bars: int):
+    def _gen_fugues_random(self, load_path: str, num_bars: int):
+        """Internal function for generating a full length fugues.
+        Args:
+            num_bars: the number of bars to be added to the original prompt.
+        """
+        def _gen_fugue(model: ChoraleBertModel, dataset: ChoraleDataset, prompt: list, num_bars: int):
+            """Given a the start of a fugue, complete it by adding num_bars more bars."""
+            # Gibbs hyperparams
+            alpha_max = 1. 
+            alpha_min = 0.05
+            num_steps = 200 
+            neta = 0.75
+            temp_max = 1.
+            temp_min = 0.5
+
+            TOKENS_PER_BAR = 5*16
+            MASK_PER_BAR = 4*16
+            MASKED_BAR = (['<T>'] + ['<M>']*4)*16
+            PAD_BAR = (['<P>']*5)*16
+
+            res = prompt.copy()[:2*TOKENS_PER_BAR] + MASKED_BAR*num_bars + ['<E>'] + PAD_BAR
+            
+            for n in range(num_steps):
+                for _ in range(num_bars):
+                    # Chose random bar number to resample
+                    mid_bar_num = random.randint(0, num_bars-1)
+                    
+                    if mid_bar_num == 0: # Case: two prompt bars included
+                        total_to_mask = MASK_PER_BAR*2
+                    elif mid_bar_num == 1: # Case: one prompt bar included
+                        total_to_mask = MASK_PER_BAR*3
+                    elif mid_bar_num == num_bars-1: # Case: one pad bar included
+                        total_to_mask = MASK_PER_BAR*3
+                    else: # Case: no prompt or pad bars included 
+                        total_to_mask = MASK_PER_BAR*4
+                        
+                    # Calculate Gibbs samplying params
+                    mask_prob = max(alpha_min,
+                                    alpha_max -
+                                    ((n*(alpha_max - alpha_min)) / (neta*num_steps))
+                                    )
+
+                    temp = temp_max + n*((temp_min - temp_max)/(num_steps)) 
+                    block_size = max(1, math.trunc(mask_prob * total_to_mask))
+                    
+                    # Get bar for resampling 
+                    if mid_bar_num == num_bars-1: # Case: there is padding at the end
+                        resample_bar = res[(mid_bar_num)*TOKENS_PER_BAR: (mid_bar_num + 4)*TOKENS_PER_BAR + 1]
+                    else: # Case: no padding
+                        resample_bar = res[(mid_bar_num)*TOKENS_PER_BAR: (mid_bar_num + 4)*TOKENS_PER_BAR] + ['<E>']
+                    
+                    resample_bar[0] = '<S>'
+                    resample_bar_enc = dataset._encode(resample_bar)
+
+                    # Gibbs step
+                    gibbs_bar_enc = gibbs_step(model, dataset, temp, block_size, resample_bar_enc, mid_bar_num, num_bars)
+                    gibbs_bar_dec = dataset.decode(gibbs_bar_enc)
+                    
+                    # Reformat
+                    if mid_bar_num != 0:
+                        gibbs_bar_dec[0] = '<T>' # Revert <S> back to <T>
+                    gibbs_bar_dec.pop() # Remove <E> tag 
+
+                    # Insert into res
+                    res[(mid_bar_num)*TOKENS_PER_BAR: (mid_bar_num + 4)*TOKENS_PER_BAR] = gibbs_bar_dec
+                
+                print(f'{n+1}/{num_steps} steps completed.')
+
+            return res
+        
+        def gibbs_step(model: ChoraleBertModel, dataset: ChoraleDataset,
+                       temp: float, block_size: int, seq: torch.tensor,
+                       mid_bar_num: int, num_bars: int):
+            """Performs a single gibbs step according to the parameters. Note
+            only works with 64len."""
+            TOKENS_PER_BAR = 5*16
+            CHORD_PER_BAR = 16
+            EQ_CHORD = [0., 1., 1., 1., 1.]
+
+            seq = torch.clone(seq) 
+            mask_key = dataset.token_to_key['<M>']
+
+            # Generate idx sampling distribution
+            if mid_bar_num == 0:
+                uniform_dist = torch.tensor([0.]*TOKENS_PER_BAR*2 + EQ_CHORD*(CHORD_PER_BAR*2) + [0.])
+            elif mid_bar_num == 1:
+                uniform_dist = torch.tensor([0.]*TOKENS_PER_BAR + EQ_CHORD*(CHORD_PER_BAR*3) + [0.])
+            elif mid_bar_num == num_bars-1:
+                uniform_dist = torch.tensor(EQ_CHORD*(CHORD_PER_BAR*3) + [0.]*TOKENS_PER_BAR + [0.])
+            else:
+                uniform_dist = torch.tensor(EQ_CHORD*(CHORD_PER_BAR*4) + [0.]) 
+
+            uniform_dist = uniform_dist / torch.linalg.norm(uniform_dist)
+
+            # Gibbs sample step
+            idx = torch.multinomial(uniform_dist, block_size, replacement=False)
+            seq[idx] = mask_key
+            logits = model.forward(seq.reshape(1, -1)) / temp # Shape (1, seq_len, vocab_len)
+            probs = torch.nn.functional.softmax(logits[0, idx, :], dim=1) # Shape (block_size, vocab_len)
+            
+            for i in range(block_size):
+                seq[idx[i]] = torch.multinomial(probs[i], 1)
+
+            return seq
+
+        dataset = self.dataset
+        model = self.model
+        save_path = self.save_path
+        
+        with open(load_path) as f:
+            prompts = json.load(f)
+            
+        for i, prompt in enumerate(prompts):
+            res = _gen_fugue(model, dataset, prompt, num_bars)
+            to_midi(res, os.path.join(save_path, f'f{i+1}.midi'))
+            print(f'Finished {i+1}/{len(prompts)}')
+            
+    def _gen_fugues_autoreg(self, load_path: str, num_bars: int):
         """Internal function for generating a full length fugues.
         Args:
             num_bars: the number of bars to be added to the original prompt.
@@ -102,9 +220,6 @@ class Sampler():
             to_midi(res, os.path.join(save_path, f'f{i+1}.midi'))
             print(f'Finished {i+1}/{len(prompts)}')
             
-            if i == 50:
-                break
-
 
 def gibbs_sample(model: ChoraleBertModel, dataset: ChoraleDataset, seq: torch.tensor):
     """Generates samples according to a simplistic gibbs sampling procedure.
@@ -118,12 +233,12 @@ def gibbs_sample(model: ChoraleBertModel, dataset: ChoraleDataset, seq: torch.te
     # Hyperparams from 'Counterpoint by Convolution' paper
     alpha_max = 1. 
     alpha_min = 0.05
-    num_steps = 200 
+    num_steps = 200
     neta = 0.75
 
     # Hyperparams for tempertature scaling
     temp_max = 1.
-    temp_min = 0.7
+    temp_min = 0.75
 
     seq = torch.clone(seq) 
     mask_key = dataset.token_to_key['<M>']
@@ -159,12 +274,16 @@ def to_midi(seq: list, save_path: str):
         save_path: save path for midi file.
     """
     if '<P>' in seq:
-        print('Sequence cannot contain <P>')
-        return
+        print('Sequences contains, <P>. Removing all such occurances...')
+        print('Original:')
+        print(seq)
+        seq = [i for i in seq if i != '<P>']
+        print('New:')
+        print(seq)
 
     midi_res = MIDIFile(removeDuplicates=False, deinterleave=False) # Error without these options
     midi_res.addProgramChange(0, 0, 0, 0) # Piano = 0, Organ = 20
-    midi_res.addTempo(0, 0, 160) # 160 BPM
+    midi_res.addTempo(0, 0, 240) # 240 BPM
     
     STATIC_TOKENS = ['<S>', '<E>', '<M>', '<T>', '<P>'] 
 
